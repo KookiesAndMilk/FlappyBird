d27a98f76852307538c3830e2058a528e39177bf
KookiesAndMilk <a7149@palmore.edu.mx>
2024-10-18 09:27:35 -0600

    avance de pantalla

--- FlappyBird.py
+++ FlappyBird.py
@@ -1,1 +1,21 @@
 #Flappy Bird en Pygame
+import pygame, sys
+pygame.init()
+screen = pygame.display.set_mode((288,512))
+clock = pygame.time.Clock()
+
+bg_surface = pygame.image.load('sprites/background-day.png').convert()
+
+floor_surface = pygame.image.load('sprites/base.png').convert()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.blit(bg_surface,(0,0))
+    screen.blit(floor_surface,(0,0))
+
+    pygame.display.update()
+    clock.tick(120)
